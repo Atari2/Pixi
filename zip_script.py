536823f0533c3aa05d74bcec674469f02a58e636
Alessio <alex.robassomero@gmail.com>
2021-04-09 12:50:39 +0200

    Added zip script and added a gitignore.

---
+++ zip_script.py
@@ -0,0 +1,17 @@
+from zipfile import ZipFile
+import glob
+from pathlib import Path
+
+executable_path = 'out/build/x86-Release/Pixi/MinSizeRel/Pixi.exe'
+cfg_editor_path = 'CFGEditor/CFG Editor/bin/Release/CFG Editor.exe'
+to_zip = [(file, file.split('\\', 1)[-1]) for file in glob.glob('Resources/**/*', recursive=True)]
+
+print(to_zip)
+
+with ZipFile('pixi.zip', 'w') as zpf:
+    for real, zipname in to_zip:
+        zpf.write(real, arcname=zipname)
+    zpf.write(cfg_editor_path, arcname=Path(cfg_editor_path).name)
+    zpf.write(executable_path, arcname=Path(executable_path).name)
+
+
